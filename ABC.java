9afafc44a9c4412f4cd3646ca13dd6287656345f
liseth <142031712+1lizzz@users.noreply.github.com>
2023-08-29 14:41:18 +0200

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,15 @@
+
+/*@author Corrales*/
+public class ABC {
+    public static void main(String[] args) {
+        //ejerc 4
+         for (char x = 'Z'; x >= 'A'; x--) {
+            for (char y = x; y >= 'A'; y--) {                                                                          
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
